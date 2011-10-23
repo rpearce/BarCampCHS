4526369c1e6a1c5d9cb677c89ad2366e2963449b
Robert Pearce <robertwaltonpearce@gmail.com>
2011-10-23 13:14:37 -0400

    Updated README

--- capybara-barcamp/find_morpheus_test.rb
+++ capybara-barcamp/find_morpheus_test.rb
@@ -10,16 +10,21 @@ class FindMorpheusTest < Test::Unit::TestCase
 
   def test_find_morpheus_quote
     fill_in 'lst-ib', :with => 'the matrix neo'
+    sleep 5
     click_link 'Neo (The Matrix)'
+    sleep 5
     click_link 'Oracle'
     click_link 'Morpheus'
+    sleep 5
     page.should have_content 'And Now That Dream is Gone from Me'
   end
 
   def test_where_in_the_world_is_morpheus
     fill_in 'lst-ib', :with => 'neo searching for morpheus'
+    sleep 5
     click_link 'Neo searching for Morpheus - YouTube'
     puts "\e[H\e[2J"
+    sleep 5
     puts "Wake up, Neo..."
     sleep 1000
   end
